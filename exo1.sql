bcaa0fddfde5ac106ffd2b411e230f9c1592c53f
Nassim Sakhri <tails01@hotmail.fr>
2023-12-12 13:58:17 +0100

    ajout exercice 3

--- exo1.sql
+++ exo1.sql
@@ -7,7 +7,7 @@ personne_id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(3),
 prenom VARCHAR(50),
 nom VARCHAR(50),
-telephone INT,
+telephone VARCHAR(10),
 email VARCHAR(50)	
 );
 
@@ -26,3 +26,21 @@ show columns from personne;
 show columns from adresse;
 
 show tables;
+
+-- Partie 2 exo 1
+
+INSERT INTO personne VALUES 
+(1,"M.","Nassim12","Sakhri","0123456789","sakhri@slhf.fr"),
+(2,"M.","Nassim0","Sakhri","0123456789","sakhri@slhf.fr"),
+(3,"M.","Nassim1","Sakhri","0123456789","sakhri@slhf.fr"),
+(4,"M.","Nassim2","Sakhri","0123456789","sakhri@slhf.fr"),
+(5,"M.","Nassim3","Sakhri","0123456789","sakhri@slhf.fr"),
+(6,"M.","toto","Sakhri","0123456789","sakhri@slhf.fr");
+
+INSERT INTO adresse VALUES (1,"rue stephenson","Roubaix",59100,1);
+
+SELECT * FROM personne;
+SELECT * FROM adresse;
+
+DELETE FROM personne WHERE personne_id = 3;
+DELETE FROM personne WHERE prenom = "toto";
